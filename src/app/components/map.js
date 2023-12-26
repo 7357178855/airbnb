4fa0f01b5b5f5810ff5eb705af0174231671e105
Pratush Tiwari <pratushtiwari@Pratushs-MacBook-Air.local>
2023-12-26 18:10:29 +0530

    vi

--- src/app/components/map.js
+++ src/app/components/map.js
@@ -2,35 +2,37 @@ import React, { useEffect } from "react";
 import { Loader } from '@googlemaps/js-api-loader';
 
 export default function Map() {
-  const mapRef = React.useRef(null);
+  // const mapRef = React.useRef(null);
 
-  useEffect(() => {
-    const initMap = async () => {
-      const loader = new Loader({
-        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY,
-        version: 'weekly'
-      });
+  // useEffect(() => {
+  //   const initMap = async () => {
+  //     const loader = new Loader({
+  //       apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY,
+  //       version: 'weekly'
+  //     });
 
-      const { Map } = await loader.importLibrary('maps');
-      const position = {
-        lat: 43.642693,
-        lng: -79.3871189
-      };
+  //     const { Map } = await loader.importLibrary('maps');
+  //     const position = {
+  //       lat: 43.642693,
+  //       lng: -79.3871189
+  //     };
 
       // map options
-      const mapOptions = {
-        center: position,
-        zoom: 17,
-        mapId: 'MY_NEXTJS_MAPID'
-      };
+  //     const mapOptions = {
+  //       center: position,
+  //       zoom: 17,
+  //       mapId: 'MY_NEXTJS_MAPID'
+  //     };
 
-      const map = new Map(mapRef.current, mapOptions);
-    };
+  //     const map = new Map(mapRef.current, mapOptions);
+  //   };
 
-    initMap();
-  }, []);
+  //   initMap();
+  // }, []);
 
   return (
-    React.createElement('div', { style: { height: '600px' }, ref: mapRef })
+//    React.createElement('div', { style: { height: '600px' }, ref: mapRef })
+React.createElement('div', { style: { height: '600px' }})
+
   );
 }
